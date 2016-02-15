3eec8479a697f6af54d3cf30080ffb43929c5c81
dccd202 <dccd202@gmail.com>
2016-02-15 01:15:36 -0500

    Create plot4.r

---
+++ plot4.r
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+Dayz <- c("1/2/2007","2/2/2007")
+RelRange <- filter((read.table("EDA/household_power_consumption.txt", header = T, sep=";", stringsAsFactors = FALSE)), Date %in% Dayz)
+RelRange$ConvDt <- strptime((paste(RelevantRange$Date, RelevantRange$Time, sep=" ")), "%m/%d/%Y %H:%M:%S")
+par(mfrow = c(2,2))
+with(RelRange, {
+  plot(RelRange$ConvDt,RelRange$Global_active_power, type = "l", xlab = "", ylab ="Global Active Power (kilowatts)")
+  plot(RelRange$ConvDt,RelRange$Voltage, type = "l", xlab = "datetime",ylab="Voltage")
+  ggplot(RelRange, aes(x=ConvDt)) + geom_line(aes(y = Sub_metering_3), color = 'blue') +geom_line(aes(y = Sub_metering_2), color = 'red') + geom_line(aes(y = Sub_metering_1), color = 'black') + ylab(label="Energy sub-metering")  
+  plot(RelRange$ConvDt,RelRange$Global_reactive_power, type = "l", xlab = "datetime")
+})

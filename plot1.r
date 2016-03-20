0fecb9ea325547d9c36a775e927d13ba66345d2c
dccd202 <dccd202@gmail.com>
2016-03-19 21:54:39 -0400

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,5 +1,5 @@
 library(dplyr)
 Dayz <- c("2/1/2007","2/2/2007")
 RelRange <- filter((read.table("EDA/household_power_consumption.txt", header = T, sep=";", na.strings=c("", "NA","?"),)), Date %in% Dayz)
-RelRange$ConvDt <- strptime((paste(RelevantRange$Date, RelevantRange$Time, sep=" ")), "%m/%d/%Y %H:%M:%S")
+RelRange$ConvDt <- strptime((paste(RelevantRange$Date, RelevantRange$Time, sep=" ")), "%d/%m/%Y %H:%M:%S")
 hist((as.numeric(RelRange$Global_active_power)), breaks =15, col="orange", xlab = "Global Active Power")
